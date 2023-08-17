6d7764f41fe48c4cc54dc6b727e275d2c5810d58
Eric Conklin <eric.s.conklin@gmail.com>
2023-08-17 17:45:30 -0400

    stuff

--- src/uicomponents/Input.jsx
+++ src/uicomponents/Input.jsx
@@ -2,13 +2,13 @@ import React from "react"
 
 import { useTheme } from "./theme"
 
-export const Input = ({ onChange, className = "input", ...props }) => {
+export const Input = React.forwardRef(({ onChange, className = "input", ...props }, ref) => {
   const doOnChange = React.useCallback(e => {
     onChange(e.target.value);
   }, [onChange]);
   const theme = useTheme();
   return (
-    <input { ...props } onChange={ doOnChange }
+    <input ref={ ref } { ...props } onChange={ doOnChange }
       className={ theme[className] }/>
   )
-}
+})
